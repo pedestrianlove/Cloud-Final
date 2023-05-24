05b0a624a0eaf66109a4eb962dec525b57612134
J.S.LI <j.s.li@gapp.nthu.edu.tw>
2023-05-24 15:47:10 +0800

    1. 將Pagination包成Component。

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,6 +4,7 @@ import { Head, Link } from '@inertiajs/vue3';
 import { ref, watch } from 'vue';
 import { Inertia } from '@inertiajs/inertia';
 import debounce from "lodash/debounce";
+import Pagination from "@/Components/Pagination.vue";
 
 let props = defineProps({
     user_list: Object,
@@ -15,7 +16,8 @@ let search = ref(props.query.search);
 watch(search, debounce(function (value) {
     Inertia.get('/dashboard', { search: value }, {
         preserveState: true,
-        replace: true
+        replace: true,
+        preserveScroll: true,
     });
 }, 300));
 </script>
@@ -107,68 +109,7 @@ watch(search, debounce(function (value) {
                         </div>
 
                         <!-- Pagination -->
-                        <div class="mt-6">
-                            <ul class="inline-flex -space-x-px">
-                                <template v-for="link in user_list.links">
-                                    <li v-if="link.label === '&laquo; Previous'">
-                                        <Link
-                                            class="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                                            v-if="link.url"
-                                            :href="link.url"
-                                        >
-                                            <span class="sr-only">Previous</span>
-                                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                                        </Link>
-                                        <span v-else
-                                              class="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                                        >
-                                            <span class="sr-only">Previous</span>
-                                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                                        </span>
-                                    </li>
-                                    <li v-else v-if="link.label === 'Next &raquo;'">
-                                        <Link
-                                            class="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                                            v-if="link.url"
-                                            :href="link.url"
-                                        >
-                                            <span class="sr-only">Next</span>
-                                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-                                        </Link>
-                                        <span v-else
-                                              class="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                                        >
-                                            <span class="sr-only">Next</span>
-                                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                                        </span>
-                                    </li>
-                                    <li v-else v-if="link.active">
-                                        <Link
-                                            class="z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
-                                            v-if="link.url"
-                                            :href="link.url"
-                                            v-html="link.label"
-                                        />
-                                        <span v-else v-html="link.label"
-                                              class="z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
-                                        >
-                                        </span>
-                                    </li>
-                                    <li v-else>
-                                        <Link
-                                            class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                                            v-if="link.url"
-                                            :href="link.url"
-                                            v-html="link.label"
-                                        />
-                                        <span v-else v-html="link.label"
-                                              class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                                        >
-                                        </span>
-                                    </li>
-                                </template>
-                            </ul>
-                        </div>
+                        <Pagination :link_list="user_list.links"/>
                         <!-- Pagination -->
                     </div>
                 </div>
